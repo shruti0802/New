4af243ac6d4a5eed27a2e1b53d2d868fe7f8a3ac
shruti0802 <shru821999@gmail.com>
2021-12-31 20:20:01 +0530

    UC2 Updated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,16 @@ public class Main{
     public static void main(String agrs[])
     {
         int IS_FULL_TIME=1;
+        int EMP_RATE_PER_HR=2;
+        int empHrs=0;
+        int empWage=0;
         double empCheck= Math.floor(Math.random() * 10)%2;
         if(empCheck == IS_FULL_TIME)
-            System.out.println("Employee is Present");
+            empHrs=8;
         else
-            System.out.println("Employee is Absent");
-        System.out.println("TEST");
+            empHrs=0;
+        empWage=empHrs* EMP_RATE_PER_HR;
+            System.out.println("Emp Wage: " + empWage);
+
     }
 }
