52105bba10a02439fc6983f3e0bf716ecd8f383f
shruti0802 <shru821999@gmail.com>
2021-12-31 22:12:10 +0530

    UC5 updated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,11 +7,14 @@ public class Main {
     public static final int IS_FULL_TIME = 2;
     public static final int EMP_RATE_PER_HR = 20;
     public static final int NUM_OF_WORKING_DAYS=2;
+    public static final int MAX_HRS_IN_MONTH=10;
 
     public static void main(String[] args) {
-        int empHrs = 0,totalEmpWage=0, empWage = 0;
-        for(int day=0; day< NUM_OF_WORKING_DAYS; day++) {
+        int empHrs = 0,totalEmpHrs=0, totalWorkingDays= 0;
+        while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+            totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+
             switch (empCheck) {
                 case IS_PART_TIME:
                     empHrs = 4;
@@ -22,11 +25,10 @@ public class Main {
                 default:
                     empHrs = 0;
             }
-            empWage = empHrs * EMP_RATE_PER_HR;
-            totalEmpWage += empWage;
-            System.out.println("Emp Wage:" + empWage);
+            totalEmpHrs+=empHrs;
+            System.out.println("Day#: " +totalWorkingDays +"Emp Hr:" +empHrs );
         }
-
+        int totalEmpWage=totalEmpHrs * EMP_RATE_PER_HR;
         System.out.println("Emp Wage: " + totalEmpWage);
     }
 }
