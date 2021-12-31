eb2297bc9d5598f35d4f916235f6a60a2d9e3045
shruti0802 <shru821999@gmail.com>
2021-12-31 21:20:02 +0530

    UC3 Updated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,17 +5,20 @@ import java.sql.SQLOutput;
 public class Main{
     public static void main(String agrs[])
     {
-        int IS_FULL_TIME=1;
-        int EMP_RATE_PER_HR=2;
+        int IS_PART_TIME=1;
+        int IS_FULL_TIME=2;
+        int EMP_RATE_PER_HR=20;
         int empHrs=0;
         int empWage=0;
-        double empCheck= Math.floor(Math.random() * 10)%2;
-        if(empCheck == IS_FULL_TIME)
+        double empCheck= Math.floor(Math.random() * 10)%3;
+        if(empCheck == IS_PART_TIME)
+            empHrs=4;
+        else if (empCheck == IS_FULL_TIME)
             empHrs=8;
         else
             empHrs=0;
         empWage=empHrs* EMP_RATE_PER_HR;
-            System.out.println("Emp Wage: " + empWage);
+        System.out.println("Emp Wage: " + empWage);
 
     }
 }
