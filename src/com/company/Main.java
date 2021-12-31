e2ed1b8b0233a867ad6772421c006caa0a3d4fa1
shruti0802 <shru821999@gmail.com>
2021-12-31 12:32:53 +0530

    UC1

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main{
+    public static void main(String agrs[])
+    {
+        int IS_FULL_TIME=1;
+        double empCheck= Math.floor(Math.random() * 10)%2;
+        if(empCheck == IS_FULL_TIME)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
+    }
+}
